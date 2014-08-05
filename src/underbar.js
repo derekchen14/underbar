a365fe3dae74b8caa8e79487ca81961d3f50e5e4
Derek Chen <derekchen14@gmail.com>
2014-08-05 16:34:52 -0700

    Completed tests for reject

--- src/underbar.js
+++ src/underbar.js
@@ -93,8 +93,11 @@ var _ = {};
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
+    var result = [];
+    _.each(collection, function(item, index){
+      if (!test(item)) {result.push(item)}
+    });
+    return result
   };
 
   // Produce a duplicate-free version of the array.
